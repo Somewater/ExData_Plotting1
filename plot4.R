da1a3a2354859d61a20160c766dee3b365a69857
Pavel Naydenov <naydenov.p.v@gmail.com>
2015-08-09 23:47:44 +0300

    fix plot4 legend

--- plot4.R
+++ plot4.R
@@ -16,7 +16,7 @@ with(data, plot(DateTime, Sub_metering_1, type = "l", xlab = "",
 with(data, lines(DateTime, Sub_metering_2, col = "red"))
 with(data, lines(DateTime, Sub_metering_3, col = "blue"))
 legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
-       col = c("black", "red", "blue"), lty = c(1,1,1), lwd = c(1,1,1))
+       col = c("black", "red", "blue"), lty = c(1,1,1), lwd = c(1,1,1), bty = "n")
 
 with(data, plot(DateTime, Global_reactive_power, type = "l", xlab = "datetime"))
 
